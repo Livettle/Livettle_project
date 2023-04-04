d00d0b7f80a2f36ef67682fc5b96c359ad42d760
Nipun Chamika <108785832+NipunChamika@users.noreply.github.com>
2023-04-04 14:01:40 +0530

    Movie Page screen

---
+++ lib/screens/MoviePage.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:sdgplivettle/widgets/MoviePageButtons.dart';
+import 'package:sdgplivettle/widgets/RecommendWidget.dart';
+
+class MoviePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.4,
+            child: Image.asset(
+              "images/cover.jpg",
+              height: 280,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.favorite_border,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.red.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "images/1.jpg",
+                              height: 250,
+                              width: 180,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 50, top: 70),
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.red,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.red.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  MoviePageButtons(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          // Movie Name from the API
+                          "The Movie Name",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "This is the smaple description of the movie, get the description from the API. get the description from the API.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  RecommendWidget(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      // // Navbar add
+      // bottomNavigationBar: CustomNavBar(),
+    );
+  }
+}

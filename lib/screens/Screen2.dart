13ce826cc40442c93eb7cf453adc3b3861d6b7be
Lojan2318 <102352545+Lojan2318@users.noreply.github.com>
2023-04-06 03:27:11 +0530

    Update Screen2.dart

--- lib/screens/Screen2.dart
+++ lib/screens/Screen2.dart
@@ -1,7 +1,7 @@
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 import 'MoviePage.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,7 +10,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:tflite/tflite.dart';
-import 'package:http/http.dart' as http;
+
 var returnValue = 'happy';
 class Movie {
   final List genre;

55eb0e25f17f87190b2aa870a23201885a5d3167
Lojan2318 <102352545+Lojan2318@users.noreply.github.com>
2023-04-06 10:25:56 +0530

    Update imdb.py

--- backend/imdb.py
+++ backend/imdb.py
@@ -8,8 +8,10 @@ from flask import Flask, request
 import json
 
 # Mapping emotions to relevant movies
+app = Flask(__name__)
 
 def detect_emotion(emotion):
+
     # IMDb URLs for Drama genre of movie against emotion Anger
     if(emotion == "angry"):
         urlhere = 'http://www.imdb.com/search/title?genres=family&title_type=feature&sort=moviemeter, asc'
@@ -28,7 +30,7 @@ def detect_emotion(emotion):
 
     # IMDb URLs for Drama genre of movie against emotion Neutral
     elif(emotion == "neutral"):
-         urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
+        urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
 
     # IMDb URLs for Drama genre of movie against emotion Sad
     elif(emotion == "sad"):
@@ -49,19 +51,18 @@ def detect_emotion(emotion):
             title = movie.find(class_='lister-item-header').a.text
             genre = re.findall(r'(\w+)', movie.find(class_='genre').text)
             image_url = movie.find('img')['loadlate']
-
             movie_info = {"title": title, "genre": genre, "image_url": image_url}
             movie_info_list.append(movie_info)
 
     return movie_info_list
 
-app = Flask(__name__)
+
 
 path = '/'
 @app.route(path, methods=['POST'])
-
 def connect():
     data = request.json
+    print(data)
     data = json.dumps(data)
     data = json.loads(data)
     sentiment_type = data["sentiment"]
@@ -69,26 +70,40 @@ def connect():
     res = detect_emotion(sentiment_type)
 
     #TODO : to get values separately for future implementation
-    for i in res:
+
+    url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+    headers = {
+        "X-RapidAPI-Key": "18de6d8512msh6ecfefddc049adcp17758bjsn8cec6d306dd5",
+        "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+    }   
+
+    content = [[] for i in range(6)]
+
+    for title in range(0,5):
+        i = res[title]
         titles = i.get("title")
-        print(titles)
-        genres = i.get("genre")
-        img_urls = i.get("image_url")
 
+        querystring = {"q":titles}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        json_data = json.loads(response.text)
+        content[title].append(json_data["d"][0]['i']['imageUrl'])
+        content[title].append(json_data["d"][0]['l'])
+        content[title].append(res[title]['genre'])
+        content[title].append(json_data["d"][0]['y'])
+        content[title].append(json_data["d"][0]['qid'])
+        print(content)
 
-    return{
-        "response ": res  #TODO: this should be optimized to return a status.
-    }
-
+    return content
+port = 8080
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host = '127.0.0.1', port = port)
 
 
 '''
 curl req to pass :
 curl -X POST http://127.0.0.1:5000/ -H 'Content-Type: application/json' -d '{"sentiment": "<mood>"}'
-
 Example :
 curl -X POST http://127.0.0.1:5000/ -H 'Content-Type: application/json' -d '{"sentiment": "angry"}'
-
+Example 2:
+Invoke-WebRequest -Method POST -Uri http://127.0.0.1:8080/ -Headers @{ 'Content-Type'='application/json' } -Body '{"sentiment": "angry"}'
 '''
